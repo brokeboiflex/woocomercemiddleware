999c14d153f99c7507208cff83630ae38739de21
brokeboiflex <pytlewskimarcel@gmail.com>
2023-08-23 15:39:39 +0200

    post supported

--- src/index.ts
+++ src/index.ts
@@ -26,17 +26,63 @@ const handler: ExportedHandler = {
 			return new Response(`Method ${request.method} not allowed.`, {
 				status: 405,
 				headers: {
-					Allow: 'GET',
+					Allow: 'GET, POST',
+				},
+			});
+		}
+
+		async function WrongContentType(request) {
+			return new Response(`Wrong content-type: ${request.headers.get('content-type')}, only 'application/json' is supported  `, {
+				status: 405,
+				headers: {
+					Allow: 'GET, POST',
 				},
 			});
 		}
 
 		const url = new URL(request.url);
 		const resource = url.searchParams.get('resource');
-		console.log(resource);
+
+		// WooCommerce request variables
+		const apiUrl = `${env.WC_URL}/wp-json/wc/v3/${resource}`;
+		const nonce = generateRandomNonce(32);
+		const timestamp = Math.floor(Date.now() / 1000).toString();
+		// Combine all the OAuth parameters as a plain object
+		const oauthParams = {
+			...{
+				oauth_consumer_key: env.WC_CONSUMER_KEY,
+				oauth_nonce: nonce,
+				oauth_signature_method: 'HMAC-SHA1',
+				oauth_timestamp: timestamp,
+				oauth_version: '1.0',
+			},
+		};
+		const method = request.method;
+
+		// Concatenate all the parameters and percent-encode them
+		const generateParameterString = (parameters: any) =>
+			Object.keys(parameters)
+				.sort()
+				.map((key) => `${encodeURIComponent(key)}=${encodeURIComponent(parameters[key])}`)
+				.join('&');
+
+		// Generate the OAuth header
+		const generateOauthHeader = (parameters: any) =>
+			`OAuth oauth_consumer_key="${encodeURIComponent(env.WC_CONSUMER_KEY)}", oauth_nonce="${encodeURIComponent(
+				nonce
+			)}", oauth_signature="${encodeURIComponent(
+				Crypto.enc.Base64.stringify(
+					// Generate the OAuth signature using HMAC-SHA1 algorithm
+					Crypto.HmacSHA1(
+						// Create the signature base string
+						`${method}&${encodeURIComponent(apiUrl)}&${encodeURIComponent(generateParameterString(parameters))}`,
+						`${encodeURIComponent(env.WC_CONSUMER_SECRET)}&`
+					)
+				)
+			)}", oauth_signature_method="HMAC-SHA1", oauth_timestamp="${encodeURIComponent(timestamp)}", oauth_version="1.0"`;
 
 		// Only GET requests work with this proxy.
-		if (request.method === 'GET') {
+		if (method === 'GET') {
 			const queryParams = Array.from(url.searchParams.entries()).filter((qp) => qp[0] !== 'resource');
 			const queryParamsObject = queryParams.reduce((acc, [key, value]) => {
 				if (value !== '') {
@@ -45,41 +91,8 @@ const handler: ExportedHandler = {
 				return acc;
 			}, {});
 
-			const apiUrl = `${env.WC_URL}/wp-json/wc/v3/${resource}`;
-			const nonce = generateRandomNonce(32);
-			const timestamp = Math.floor(Date.now() / 1000).toString();
-
-			// Combine all the OAuth parameters as a plain object
-			const params = {
-				...queryParamsObject,
-				...{
-					oauth_consumer_key: env.WC_CONSUMER_KEY,
-					oauth_nonce: nonce,
-					oauth_signature_method: 'HMAC-SHA1',
-					oauth_timestamp: timestamp,
-					oauth_version: '1.0',
-				},
-			};
-
-			// Concatenate all the parameters and percent-encode them
-			const parameterString = Object.keys(params)
-				.sort()
-				.map((key) => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
-				.join('&');
-			// Create the signature base string
-			const method = request.method.toUpperCase();
-			const signatureBaseString = `${method}&${encodeURIComponent(apiUrl)}&${encodeURIComponent(parameterString)}`;
-
-			// Generate the OAuth signature using HMAC-SHA1 algorithm
-			const signature = Crypto.HmacSHA1(signatureBaseString, `${encodeURIComponent(env.WC_CONSUMER_SECRET)}&`);
-			const oauthSignature = Crypto.enc.Base64.stringify(signature);
-
-			// Generate the OAuth header
-			const oauthHeader = `OAuth oauth_consumer_key="${encodeURIComponent(env.WC_CONSUMER_KEY)}", oauth_nonce="${encodeURIComponent(
-				nonce
-			)}", oauth_signature="${encodeURIComponent(
-				oauthSignature
-			)}", oauth_signature_method="HMAC-SHA1", oauth_timestamp="${encodeURIComponent(timestamp)}", oauth_version="1.0"`;
+			// Combine parameters
+			const oauthHeader = generateOauthHeader({ ...oauthParams, ...queryParamsObject });
 
 			const parameterdURL = `${apiUrl}${
 				queryParams.length > 0
@@ -90,7 +103,6 @@ const handler: ExportedHandler = {
 							.join('&')
 					: ''
 			}`;
-			console.log(parameterdURL);
 
 			// Make a GET request using fetch and include the Authorization header
 			const response = await fetch(parameterdURL, {
@@ -100,8 +112,7 @@ const handler: ExportedHandler = {
 				},
 			});
 
-			// Get the response body as text or JSON, depending on the API response type
-			const data = await response.json(); // Use response.text() if the response is not in JSON format
+			const data = await response.json();
 
 			// Create a new Response with the fetched data and return it
 			return new Response(JSON.stringify(data), {
@@ -114,8 +125,42 @@ const handler: ExportedHandler = {
 					'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
 				},
 			});
-		} else if (request.method === 'POST') {
-			// Will work on it when I need it ;)
+		} else if (method === 'OPTIONS') {
+			return new Response(null, {
+				headers: {
+					'Access-Control-Allow-Origin': 'http://localhost:3000', // Replace with your client's URL
+					'Access-Control-Allow-Methods': 'GET, POST', // Allowed methods
+					'Access-Control-Allow-Headers': 'Content-Type', // Allowed headers
+				},
+			});
+		} else if (method === 'POST') {
+			const contentType = request.headers.get('content-type');
+			if (contentType && contentType.includes('application/json')) {
+				const requestData = await request.json();
+				const oauthHeader = generateOauthHeader(oauthParams);
+
+				const response = await fetch(apiUrl, {
+					method: 'POST',
+					headers: {
+						Authorization: oauthHeader,
+						'Content-Type': 'application/json',
+					},
+					body: JSON.stringify(requestData),
+				});
+
+				const responseData = await response.json();
+
+				return new Response(JSON.stringify(responseData), {
+					status: response.status,
+					headers: {
+						// Add any required headers for the response here
+						'Content-Type': 'application/json', // Adjust the content type based on your API response
+						'Access-Control-Allow-Origin': 'http://localhost:3000', // Replace with your client's URL
+						'Access-Control-Allow-Methods': 'GET, POST',
+						'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+					},
+				});
+			} else return WrongContentType(request);
 		} else return MethodNotAllowed(request);
 	},
 };
